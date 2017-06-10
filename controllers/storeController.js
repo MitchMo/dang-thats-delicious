a598520e893f49e92d80fcbe14f987b08e54892e
Mitch Moore <mamoore9369@gmail.com>
2017-06-10 10:56:16 -0400

    Module 7 Finished

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter: function(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    }
+    else {
+      next({message: 'That file type isnt\' allowed you twat!'}, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -7,6 +23,25 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  //check if there is no new file to resize
+  if(!req.file) {
+    next(); //Skip to next middleware
+    return;
+  }
+
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`
+  //resize the image properly
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  //Once file has been resized and saved, skip to next middleware
+  next();
 };
 
 exports.createStore = async (req, res) => {
@@ -30,6 +65,8 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  //Set location data to be a point
+  req.body.location.type = 'Point';
   //1. Find Store and update if exists
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, //return the new store instead of the old one
@@ -38,4 +75,23 @@ exports.updateStore = async (req, res) => {
   req.flash('success', `Successfully updated ${store.name} <a href="/stores/${store.slug}">View Store</a>`);
   //2. Redirect them to the store and respond with success error
   res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  //1. Query database to find store based off slug in URL
+  const store = await Store.findOne({slug: req.params.slug});
+  if(!store) { return next(); }
+  //3. If store is there, render view with the store as the model
+  res.render('store', { title: `${store.name}` , store});
+};
+
+exports.getStoresByTag = async (req, res) => {
+  //1. Get A list of all the stores
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true }
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+
+  res.render('tag', { title: 'Tags' , tags, tag, stores});
 };
--- models/Store.js
+++ models/Store.js
@@ -13,10 +13,29 @@ const storeSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  tags: [String]
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!'
+    }],
+    address: {
+      type: String,
+      required: 'you must supply an address!'
+    }
+  },
+  photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
 
   if(!this.isModified('name')) {
     next(); //skip it
@@ -24,9 +43,26 @@ storeSchema.pre('save', function(next) {
   }
 
   this.slug = slug(this.name);
+  //find if any other stores have the same slug
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+
+  const storesWithSlug = await this.constructor.find({slug: slugRegEx});
+
+  if(storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
+
   next();
 
   // TODO: Make slugs more unique. In case two stores have the same name
 });
+
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
+};
 
 module.exports = mongoose.model('Store', storeSchema);
